208fd2dae71523ad1605b902c5dbb0a607acf021
rizzkingmew <mirtskhulavakonstantine@gmail.com>
2025-03-07 20:36:20 +0400

    kosta

---
+++ day 04/homework/index.py
@@ -0,0 +1,23 @@
+x = 69
+y = -420
+z = 2763
+
+print(x)
+print(y)
+print(z)
+print(x + y)
+
+message1 = "i"
+message2 = "code stuff"
+print(message1  +  message2)
+
+kosta = "chill"
+print(kosta)
+kosta = "i take my words back"
+print(kosta)
+
+number = "5"
+print("Result: " + number)
+
+number = True
+print("Result: " + str(number))
