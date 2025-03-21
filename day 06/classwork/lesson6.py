35227c61b0a0d37dbc5b0c3045ac755306dbdcad
rizzkingmew <mirtskhulavakonstantine@gmail.com>
2025-03-21 19:01:38 +0400

    kosta

---
+++ day 06/classwork/lesson6.py
@@ -0,0 +1,31 @@
+print('hello world')
+age = input('please input your age: ')
+print(type(age))
+
+
+age = input("ur age: ")
+print(age)
+
+name = input("please input your name")
+print("Hello," , name)
+
+
+
+
+
+
+num1 = float(input("put the first number: "))
+num2 = float(input("put the scond number: "))
+
+
+print("additiom:", num1 + num2)
+print("subtraction:", num1 - num2)
+print("multiplication:", num1 * num2)
+print("division:", num1 / num2)
+
+
+name = input("your name: ")
+age = input("your age: ")
+
+
+print("Hello", name + ",", "you are", age, "years old.")
