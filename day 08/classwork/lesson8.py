cb7768aaffef4f90953a39f2073cb93accf7c066
rizzkingmew <mirtskhulavakonstantine@gmail.com>
2025-04-05 17:57:03 +0400

    kosta

---
+++ day 08/classwork/lesson8.py
@@ -0,0 +1,40 @@
+
+# First Homework
+
+num1 = float(input("put the first number that comes up to your mind: "))
+num2 = float(input("great! now, put the second number that comes up to your mind: "))
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+# Second Homework
+
+num3 = 10
+num4 = 5
+
+print(num3 == 10)  # True
+print(num3 != 10)  # True
+print(num3 > num4)  # True
+print(num3 >= 10)  # True
+print(num4 < num3)  # True
+
+print(num3 == 5)   # False
+print(num4 > 10)   # False
+print(num3 < 5)    # False
+print(num3 != 10)  # False
+print(num4 >= 10)  # False
+
+# Third Homework
+
+my_height = float(input("put your height: "))
+friend_height = float(input("good! now put your friends height: "))
+
+print("you are taller than your friend:", my_height > friend_height)
+print("your friend is taller than you:", friend_height > my_height)
+print("You and your friend are the same height:", my_height == friend_height)
+
+
---
+++ day 08/homework/goahomework.py
@@ -0,0 +1,25 @@
+# 1. better than
+x = 15
+y = 10
+print(x > y)  # its true because 15 is better than 10
+
+# 2. less than
+x = 5
+y = 10
+print(x < y)  # its true because 5 is less than 10
+
+# 3. equal numbers
+x = 10
+y = 10
+print(x == y)  # its true because 10 and 10 are equal to each other
+
+# 4. not equal numbers
+x = 7
+y = 10
+print(x != y)  # its true because 7 and 10 are not equal
+
+# 5. if more then 10 code
+num = int(input("შეიტანეთ რიცხვი: "))  # this asks to put a number.
+print(num > 10)  # if the number that you wrote is more than 10 then its true. if not, then its false
+
+
