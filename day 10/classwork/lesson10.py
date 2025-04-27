bbcfaff34a52ec61b4e99126874f4022bc42b02f
rizzkingmew <mirtskhulavakonstantine@gmail.com>
2025-04-27 11:22:07 +0400

    kosta

---
+++ day 10/classwork/lesson10.py
@@ -0,0 +1,29 @@
+# REMEMBER!
+
+# > - greater than
+# < - less than
+# >= - greater than or equal to
+# <= - less than or equal to
+# == - equal to
+# != - not equal to
+
+#Example:
+
+# x = 5
+# y = 10
+# print(x > y) # False
+# print(x < y) # True
+# print(x >= y) # False
+# print(x <= y) # True
+# print(x == y) # False
+# print(x != y) # True
+
+# CLasswork
+print(True and False or False and False or True or True or False)
+
+# the answer: True
+
+# That is, first not happens, then and, finally or.
+# That is, when Python executes the code, it will first "determine" and, and then or.
+
+# 1.) * (multiplication)
