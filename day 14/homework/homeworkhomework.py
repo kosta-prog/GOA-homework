3a5d4fee17dbb2c5a0522175f656547d600221e6
rizzkingmew <mirtskhulavakonstantine@gmail.com>
2025-05-25 10:35:09 +0400

    kosta

---
+++ day 14/homework/homeworkhomework.py
@@ -0,0 +1,31 @@
+#1 სავარჯიშო
+
+a = 13
+b = 33
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+#2 სავარჯიშო
+
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
+
+#3 სავარჯიშო
+
+print(bool(5 > 3))
+print(5 + 3)
+
+#4 სავარჯიშო
+
+print(True and not False and (False or True) and True or not False and False and not True and (True or False) or (True and False))
+
+#True
